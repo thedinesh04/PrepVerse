ab2dc2e87aeb1623f742ecfd81e4fc43d5c5b095
thedinesh04 <naikd0153@gmail.com>
2025-07-13 12:23:38 +0530

    Set-up app, file and folder structure routind and auth pages

---
+++ Components/FormField.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/Components/ui/form";
+import {Input} from "@/Components/ui/input";
+import {Control, Path, Controller, FieldValues} from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues>{
+    control : Control<T>;
+    name : Path<T>;
+    label : string;
+    placeholder? : string;
+    type ? : "text" | "password" | "email" | "file";
+
+}
+
+const FormField = ({control, name, label, placeholder, type = "text"} : FormFieldProps<T>) => {
+    return (
+        <Controller
+            control={control}
+            name ={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="label">{label}</FormLabel>
+                    <FormControl>
+                        <Input
+                            className={"input"}
+                           placeholder={placeholder}
+                            type={type}
+                           {...field} />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    )
+}
+export default FormField
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuthForm from "@/Components/AuthForm";
+
+const Page = () => {
+    return (
+        <AuthForm type="sign-in" />
+    )
+}
+export default Page
